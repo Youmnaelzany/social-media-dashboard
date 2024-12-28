85c9502bf756ebd80862ad50fedff6cb3e41a943
Youmna <youmnaprowork@gmail.com>
2024-12-28 20:51:19 +0200

    Remove unused import in ModeToggle component

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -1,8 +1,7 @@
 "use client"
 
 import * as React from "react"
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { Moon, MoonIcon, Sun } from "lucide-react"
+import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 
 import { Button } from "@/components/ui/button"
